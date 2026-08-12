5c721622ef0a93983e28766e6f3bee9114fe0b8e
chuongmaiisme <chuongmaiisme@gmail.com>
2026-08-12 22:13:01 +0700

    4h

--- Source/MeleeCombatOnline/Private/Character/MCOCharacter_Player.cpp
+++ Source/MeleeCombatOnline/Private/Character/MCOCharacter_Player.cpp
@@ -3,3 +3,76 @@
 
 #include "Character/MCOCharacter_Player.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+AMCOCharacter_Player::AMCOCharacter_Player()
+{
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+
+	CameraComp->SetupAttachment(SpringArmComp);
+	SpringArmComp->SetupAttachment(GetRootComponent());
+}
+
+void AMCOCharacter_Player::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+	// Clients: Local Player
+	// Local Player -> Player Controller -> Character
+	// Add IMC by Local Player
+
+
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller possesses character"));
+
+	AddMappingContext();
+}
+
+void AMCOCharacter_Player::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComp->BindAction(InputAction_Jump, ETriggerEvent::Triggered, this, &AMCOCharacter_Player::HandleActionJump);
+}
+
+void AMCOCharacter_Player::AddMappingContext()
+{
+	APlayerController* MyPlayerController = GetController<APlayerController>();
+
+	if (MyPlayerController == nullptr)
+	{
+		return;
+	}
+
+	ULocalPlayer* MyLocalPlayer = MyPlayerController->GetLocalPlayer();
+
+	if (MyLocalPlayer == nullptr)
+	{
+		return;
+	}
+
+	UEnhancedInputLocalPlayerSubsystem* EnhancedInputSubsystem =
+		MyLocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+
+
+	if (EnhancedInputSubsystem == nullptr)
+	{
+		return;
+	}
+
+	// Main Logic
+	EnhancedInputSubsystem->RemoveMappingContext(IMC_Default);
+	EnhancedInputSubsystem->AddMappingContext(IMC_Default, 0);
+}
+
+void AMCOCharacter_Player::HandleActionJump()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Handle Action Jump"));
+	Jump();
+}
--- Source/MeleeCombatOnline/Public/Character/MCOCharacter_Player.h
+++ Source/MeleeCombatOnline/Public/Character/MCOCharacter_Player.h
@@ -6,7 +6,10 @@
 #include "Character/MCOCharacter.h"
 #include "MCOCharacter_Player.generated.h"
 
+class USpringArmComponent;
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -14,7 +17,31 @@ UCLASS()
 class MELEECOMBATONLINE_API AMCOCharacter_Player : public AMCOCharacter
 {
 	GENERATED_BODY()
-	
-	UPROPERTY()
-	UCameraComponent* CameraComp;
+
+public:
+	AMCOCharacter_Player();
+
+	virtual void PawnClientRestart() override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private: // Method
+	void AddMappingContext();
+	void HandleActionJump();
+
+private: // Property
+	UPROPERTY(visibleDefaultsOnly)
+	TObjectPtr<UCameraComponent> CameraComp;
+
+	UPROPERTY(VisibleDefaultsOnly)
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+
+	// TObjectPtr<UInputMappingContext> IMC_Default;
+
+	// Edit
+	UPROPERTY(EditDefaultsOnly, Category="MCO Settings | Input")
+	TObjectPtr<UInputMappingContext> IMC_Default;
+
+	UPROPERTY(EditDefaultsOnly, Category="MCO Settings | Input")
+	TObjectPtr<UInputAction> InputAction_Jump;
 };
